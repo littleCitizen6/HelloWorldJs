01617dfef2306558579477df1a152e4c7298e662
unknown <theLittleCitizen6@gmail.com>
2020-09-13 23:45:44 -0700

    finish

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+function getName() {
+    var person = prompt("Please enter your name");
+    console.log('hello world! Good morning ${person}');
+    alert(`Hello World! Good morning ${person}`);
+}
